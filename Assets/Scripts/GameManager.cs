8200aa12917991c25c510664182286dd947ebd66
lwhite14 <55700734+lwhite14@users.noreply.github.com>
2019-11-07 00:17:14 +0000

    Menu scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Zombie Shooter Level 1");
+    }
+}
