fd1d2aba29d7fb1bc771df7cb7db76557724492a
lwhite14 <55700734+lwhite14@users.noreply.github.com>
2019-12-16 21:16:41 +0000

    Death screen created

--- Assets/Scripts/DestroyOnDie.cs
+++ Assets/Scripts/DestroyOnDie.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class DestroyOnDie : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     public void Die()
     {
         Destroy(gameObject);
+    }
+
+    public void playerDie() 
+    {
+        Destroy(gameObject);
+
+        GameObject deathPanel = GameObject.Find("Death Panel");
+        GameObject deathText = GameObject.Find("Death Text");
+        GameObject deathButton = GameObject.Find("Death Button");
+        Animator panelAnim = deathPanel.GetComponent<Animator>();
+        Animator textAnim = deathText.GetComponent<Animator>();
+        Animator buttonAnim = deathButton.GetComponent<Animator>();
+        panelAnim.SetBool("isDead", true);
+        textAnim.SetBool("isDead", true);
+        buttonAnim.SetBool("isDead", true);
+
+
+
+
+
+        //Image image = deathPanel.GetComponent<Image>();
+
+        //Color c = image.color;
+        //c.a = 255;
+        //image.color = c;
+
+        //Debug.Log("yeet");
     }
 }
