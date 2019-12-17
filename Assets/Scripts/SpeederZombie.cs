03b48cab7a31564919d98e13a2be281f46ce467a
lwhite14 <55700734+lwhite14@users.noreply.github.com>
2019-12-17 15:23:32 +0000

    Created and animated new type of zombie

---
+++ Assets/Scripts/SpeederZombie.cs
@@ -0,0 +1,53 @@
+﻿using Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeederZombie : MonoBehaviour
+{
+    AILerp lerp;
+    int counter = 0;
+    bool isLow = true;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        AILerp lerp = gameObject.GetComponent<AILerp>();
+        if (isLow)
+        {
+            if (counter > 200)
+            {
+                updateSpeed(isLow, lerp);
+                isLow = false;
+                counter = 0;
+            }
+        }
+        else 
+        {
+            if (counter > 80) 
+            {
+                updateSpeed(isLow, lerp);
+                isLow = true;
+                counter = 0;
+            }
+        }
+
+
+        counter += 1;
+    }
+
+    void updateSpeed(bool isLow, AILerp lerp) 
+    {
+        if (isLow)
+        {
+            lerp.speed = 10;
+        }
+        else 
+        {
+            lerp.speed = 1;
+        }
+    }
+}
