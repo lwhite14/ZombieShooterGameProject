1ca50942d3dff3c766e3e8228dcc007f8f282f52
lwhite14 <55700734+lwhite14@users.noreply.github.com>
2019-12-17 04:58:12 +0000

    Added new player sprite
    Also made animations and removed 'smooth look 2d'

--- Assets/Scripts/TopDownCharacterController2D.cs
+++ Assets/Scripts/TopDownCharacterController2D.cs
@@ -4,14 +4,36 @@ public class TopDownCharacterController2D : MonoBehaviour
 {
     public float speed = 5.0f;
     Rigidbody2D rigidbody2D;
+    private Animator walkAnim;
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        walkAnim = GetComponent<Animator>();
     }
     void FixedUpdate()
     {
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
+
+        if ((x != 0) | (y != 0))
+        {
+            if (y > 0)
+            {
+                walkAnim.SetBool("isWalkingForward", false);
+                walkAnim.SetBool("isWalkingBackward", true);
+            }
+            else
+            {
+                walkAnim.SetBool("isWalkingForward", true);
+                walkAnim.SetBool("isWalkingBackward", false);
+            }
+        }
+        else
+        {
+            walkAnim.SetBool("isWalkingForward", false);
+            walkAnim.SetBool("isWalkingBackward", false);
+        }
+
         rigidbody2D.velocity = new Vector2(x, y) * speed;
         rigidbody2D.angularVelocity = 0.0f;
     }}
