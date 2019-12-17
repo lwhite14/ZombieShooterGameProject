758ccfd28ea692ebbb56ebbd9fbefd6d762eaff1
lwhite14 <55700734+lwhite14@users.noreply.github.com>
2019-12-17 00:25:42 +0000

    Fixed knockback on player collide bug

--- Assets/Scripts/Knockback.cs
+++ Assets/Scripts/Knockback.cs
@@ -38,25 +38,29 @@ public class Knockback : MonoBehaviour
 
     public void OnTriggerEnter2D(Collider2D other)
     {
-        Vector2 difference = transform.position - playerObj.transform.position;
-
-        int neg = 1;
-        if (difference.x < 0) 
+        if (other.tag != "Player")
         {
-            neg = -1;
+
+            Vector2 difference = transform.position - playerObj.transform.position;
+
+            int neg = 1;
+            if (difference.x < 0)
+            {
+                neg = -1;
+            }
+
+            double angle = getAngle(difference.y, difference.x);
+            double pushX = getAd(angle) * neg;
+            double pushY = getOp(angle) * neg;
+
+            enemyObj.GetComponent<AILerp>().enabled = false;
+            transform.position = new Vector2(transform.position.x + (float)pushX, transform.position.y + (float)pushY);
+            enemyObj.GetComponent<AILerp>().enabled = true;
+
+            var enemyRenderer = enemyObj.GetComponent<Renderer>();
+            enemyRenderer.material.SetColor("_Color", Color.red);
+            isHurt = true;
         }
-
-        double angle = getAngle(difference.y, difference.x);
-        double pushX = getAd(angle) * neg;
-        double pushY = getOp(angle) * neg;
-
-        enemyObj.GetComponent<AILerp>().enabled = false;
-        transform.position = new Vector2(transform.position.x + (float)pushX, transform.position.y + (float)pushY);
-        enemyObj.GetComponent<AILerp>().enabled = true;
-
-        var enemyRenderer = enemyObj.GetComponent<Renderer>();
-        enemyRenderer.material.SetColor("_Color", Color.red);
-        isHurt = true;
     }
 
     private static double getAd(double ang)
