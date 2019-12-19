b5a1843c6c1a61ff093a82375f2f4c8389be6742
lwhite14 <55700734+lwhite14@users.noreply.github.com>
2019-12-19 09:15:11 +0000

    Death sounds added (basic)

---
+++ Assets/Scripts/AudioControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioControl : MonoBehaviour
+{
+    public AudioClip clip;
+
+    public void PlayOnDie() 
+    {
+
+        AudioSource.PlayClipAtPoint(clip, new Vector3(0, 0, 0));       
+
+    }
+
+}
