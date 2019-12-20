daf61e873d4273c6823d9bca10d93a628f65f0f2
lwhite14 <55700734+lwhite14@users.noreply.github.com>
2019-12-20 11:12:06 +0000

    Gameobject to spawn zombies at certain point of the map

---
+++ Assets/Scripts/DetectAndActivate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectAndActivate : MonoBehaviour
+{
+    public GameObject[] spawners;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Wall Collision")
+        {
+            foreach (GameObject spawner in spawners)
+                spawner.SetActive(true);
+        }
+    }
+}
