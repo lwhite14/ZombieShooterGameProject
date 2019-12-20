9a9b5daef4e9871e60195af34c0d18dce56d95e1
lwhite14 <55700734+lwhite14@users.noreply.github.com>
2019-12-20 06:40:04 +0000

    Added npc (not interactable)

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    float y;
+    public float speed = 2;
+    public float lowestY;
+    public float highestY;
+    float realSpeed;
+    bool isWalkingForward;
+
+    void Start()
+    {
+        realSpeed = speed / 100;
+    }
+
+    void Update()
+    {
+        if (isWalkingForward)
+        {
+            y -= realSpeed;
+        }
+        else 
+        {
+            y += realSpeed;
+        }
+
+        if (y < lowestY) 
+        {
+            isWalkingForward = false;
+        }
+
+        if (y > highestY)
+        {
+            isWalkingForward = true;
+        }
+
+
+        transform.position = new Vector2(transform.position.x, y);
+    }
+}
