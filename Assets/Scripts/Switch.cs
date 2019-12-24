409323d595fc3e6144929d1bb72b327e3e0fc45f
lwhite14 <55700734+lwhite14@users.noreply.github.com>
2019-12-24 11:13:03 +0000

    Completed level 2, created switch

---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    public AudioClip activateClip;
+    public GameObject gate;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Ball") 
+        {
+            AudioSource.PlayClipAtPoint(activateClip, transform.position);
+            Destroy(gate);
+            Destroy(GameObject.Find("Ball"));
+        }
+    }
+
+
+}
