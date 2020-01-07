7a6d3bb0dd907fd61eea26da648dc8877d994f0d
lwhite14 <55700734+lwhite14@users.noreply.github.com>
2020-01-07 18:37:40 +0000

    Game over scene text updated
    Now if you make it to the end the game over text reads 'you survived'

---
+++ Assets/Scripts/EndGameText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndGameText : MonoBehaviour
+{
+    public Text gameOverText;
+    int health;
+    void Start()
+    {
+        health = PlayerPrefs.GetInt("Health");
+        if (health == 0)
+        {
+            gameOverText.text = "You Died!";
+        }
+        else 
+        {
+            gameOverText.text = "You Survived!";
+        }
+    }
+
+}
